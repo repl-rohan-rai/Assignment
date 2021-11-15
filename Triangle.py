697ccfadabcf06bb87b976b774cb0c335db32445
repl-rohan-rai <91752908+repl-rohan-rai@users.noreply.github.com>
2021-11-15 16:09:19 +0530

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -1,15 +1,17 @@
-def triangle(l):
-    list_length=len(l)
+def triangle(triangle_list):
+    list_length=len(triangle_list)
+    if all(isinstance(x, int) for x in triangle_list)!=True:
+        return "Input Integer value only"
     if list_length==1:
-        return l[0]
+        return triangle_list[0]
     elif list_length==0:
         return "List cannot be empty"
     else:
-        path_value=l[0][0]
+        path_value=triangle_list[0][0]
         j=0
         for i in range(1,list_length):
-            path_value+=min(l[i][j],l[i][j+1])
-            j=l[i].index(min(l[i][j],l[i][j+1]))
+            path_value+=min(triangle_list[i][j],triangle_list[i][j+1])
+            j=l[i].index(min(triangle_list[i][j],triangle_list[i][j+1]))
         return path_value
-l=[[2],[3,7],[8,5,6],[6,1,9,3]]
-print(triangle(l))
+triangle_list=[[2],[3,7],[8,5,6],[6,1,9,3]]
+print(triangle(triangle_list))
